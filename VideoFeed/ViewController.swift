081a33d15cd7b67dc7a3d45799787f02fa411d75
Javier Ramirez <javik4d@gmail.com>
2021-09-19 18:14:01 -0600

    Initial Commit

---
+++ VideoFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VideoFeed
+//
+//  Created by Javier Ramirez on 19/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
